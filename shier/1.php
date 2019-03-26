a714924c13b27a267ca2c23c104ae5e90a8d612a
2777893479@qq.com <2777893479@qq.com>
2019-03-26 09:16:31 +0800

    shier

---
+++ shier/1.php
@@ -0,0 +1,18 @@
+<?php
+// $str = 5;
+// echo str($str);
+// function str($str){
+// 	$arr = range(1,$str);
+// 	// print_r($arr);
+// 	$arr = array_sum($arr);
+// 	// print_r($arr);
+// 	return $arr;
+// }
+
+
+
+
+
+
+
+?>
